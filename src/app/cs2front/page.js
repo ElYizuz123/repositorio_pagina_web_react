3ec9ae083771403dff53e782bc02f89a457a8bd3
CarlosHernandez14 <carloskvpchc@gmail.com>
2023-10-25 11:51:09 -0600

    Se cambio a imagen Valorant

--- src/app/cs2front/page.js
+++ src/app/cs2front/page.js
@@ -5,7 +5,7 @@ function Page() {
     <div>
       <h2>Ya jugaste al CS2</h2>
       <img
-        src="https://i.imgur.com/hjpEYZz.png"
+        src="https://images4.alphacoders.com/131/1319447.jpeg"
         alt="CS2"
         style={{ maxWidth: '100%' }} // Opcional: Ajusta el ancho de la imagen
       />
