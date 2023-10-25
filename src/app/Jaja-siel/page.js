18ee10f5e710987b83a02b43a55bacb0cdc03788
Jsiel <jasiel.huerta@outlook.es>
2023-10-25 12:52:25 -0600

    add a video

--- src/app/Jaja-siel/page.js
+++ src/app/Jaja-siel/page.js
@@ -1,14 +1,11 @@
 import React from 'react'
+import ReactPlayer from 'react-player';
 
 const page = () => {
   return (
     <div>
         <h1>Yo no</h1>
-        <img
-        src="https://cdn.pixabay.com/photo/2023/09/05/16/39/sunrise-8235461_1280.jpg"
-        alt="Yo"
-        style={{ maxWidth: '100%' }} 
-      />
+        <ReactPlayer url='https://youtu.be/dQw4w9WgXcQ'/>
     </div>
   )
 }
