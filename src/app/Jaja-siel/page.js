960212289741e968664da37589dcc8bd21020d45
Yisus <jose.jesus.zarate.garcia@outlook.com>
2023-10-25 12:49:10 -0600

    Mensaje

---
+++ src/app/Jaja-siel/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Yo no</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/{sY3L72fuvf0}" frameborder="0" allowfullscreen></iframe>
+      
+    </div>
+  )
+}
+
+export default page
