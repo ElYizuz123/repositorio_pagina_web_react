28b1dd16498df5c91658b3cf2e93de07bb9a366b
Octavio <opvz44@outlook.com>
2023-10-25 12:56:53 -0600

    Puse el video en el centro

--- src/app/octavio/page.js
+++ src/app/octavio/page.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        <h1>Quien jala unas del Counter Strike?</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/1stQ6r0VPKI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/1stQ6r0VPKI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
+
   )
 }
 
