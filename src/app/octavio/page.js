7f336c5c8eb57dc4e97d965e328caf53372315dd
Octavio <opvz44@outlook.com>
2023-10-25 11:39:12 -0600

    Agregue Una imagen

--- src/app/octavio/page.js
+++ src/app/octavio/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div><h1>Quien jala unas del Counter Strike?</h1></div>
+    <div>
+        <h1>Quien jala unas del Counter Strike?</h1>
+        <img src="https://cdn.akamai.steamstatic.com/apps/csgo/images/csgo_react/social/cs2.jpg" ></img>
+    </div>
   )
 }
 
