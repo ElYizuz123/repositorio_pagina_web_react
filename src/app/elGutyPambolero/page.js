84124bf8ccb99574790376daa1b79741f5294a09
elGutyPambolero <alexyanguie5@gmail.com>
2023-10-25 12:01:24 -0600

    IMAGEN DEL MEJOR JUGADOR DEL MUNDO

--- src/app/elGutyPambolero/page.js
+++ src/app/elGutyPambolero/page.js
@@ -9,6 +9,8 @@ const elGutyPambolero = () => {
         <h1>
             El Chaquito ya metio un golazo
         </h1>
+        <img src="https://storage.googleapis.com/static.elsoldemexico.com.mx/elesto/2023/03/Santi-Gimenez-marco-el-primer-minuto-de-juego.-770x450.jpg">
+        </img>
     </div>
   )
 }
